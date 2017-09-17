0d3ab3f5b05d9655dd3d4ef080804333b4e2f9a0
J Fernandez <jffernan@comcast.net>
2017-09-17 18:10:54 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // IMPORTANT!!!! uncomment the lines below to make your tests work
-/*
+const meInReact
+  React.createElement('div', { className: 'me' },
+    React.createElement('h1', {}, 'An Awesome Person'),
+    React.createElement('p', {}, 'Who is learning React'),
+    React.createElement('ul', {},
+      [
+        React.createElement('li', {}, 'Javascript'),
+        React.createElement('li', {}, 'React'),
+        React.createElement('li', {}, 'Movies'),
+        React.createElement('li', {}, 'Ice Cream')
+      ]
+    )
+  );
+
 ReactDOM.render(
-  meInReact, 
+  meInReact,
   document.getElementById('root')
 );
 
 export default meInReact
-*/
