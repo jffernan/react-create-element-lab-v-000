421d8c59a10ab7f98789a8e39f07dd8018421080
J Fernandez <jffernan@comcast.net>
2017-09-17 18:21:39 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ const meInReact =
   React.createElement('div', { className: 'me' },
     React.createElement('h1', {}, 'An Awesome Person'),
     React.createElement('p', {}, 'Who is learning React'),
-    React.createElement('ul', {},
+    React.createElement('ul', { className: 'my-interests' },
       [
-        React.createElement('li', {}, 'Javascript'),
+        React.createElement('li', {}, 'JavaScript'),
         React.createElement('li', {}, 'React'),
         React.createElement('li', {}, 'Movies'),
         React.createElement('li', {}, 'Ice Cream')
